f8998c5d586e6f0698f9817344ec39da1ef6a3fd
anjali-2504 <anjaliraj25022002@gmail.com>
2021-07-10 16:34:06 +0530

    old files added

--- TASK2/task_2.py
+++ TASK2/task_2.py
@@ -25,40 +25,46 @@ while True:
     capx=frm.shape[1]
     img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img3 = np.zeros_like(frm)
-    if(t==1 or(yi==0)):
+    if(t==1 or(yi<=20)): #if y coordinate of ball (top-left) wrt frame is 0 ---> left wall
         k2=1
-    if (xi+hi>=capy-10):
+        print("BALL HITS THE LEFT WALL")
+    if (xi+hi>=capy-20):
+        print("GAME OVER TRY AGAIN")
         break
-    if(xi+hi>=x-10 and xi+hi<=x+w and yi+ki>=y-10 and yi+ki<=y+h and k1==1 and k2==1):
+    if(xi+hi>=y-20 and xi+hi<=y+h and yi+ki>=x-20 and yi+ki<=x+w and k1==1 and k2==1):
         k1=-1
         k2=1
-        print("done")
-    if(xi+hi>=x-10 and xi+hi<=x+w and yi+ki>=y-10 and yi+ki<=y+h and k1==1 and k2==-1):
+        print("BALL HITS YOUR FACE")
+       # print("done")
+    if(xi+hi>=y-20 and xi+hi<=y+h and yi>=x-20 and yi<=x+w and k1==1 and k2==-1):
         k1=-1
-        k2=-1    
- 
-    if(yi+ki>=capx-10):
+        k2=-1  
+        print("BALL HITS YOUR FACE AGAIN")
+        #print("done")  
+  
+    if(yi+hi>=capx-20):
         k2=-1 
-        yi-=(yi+ki-capx+1)
+        yi-=(yi+hi-capx+1)
+        print("BALL HITS RIGHT WALL")
     if(xi==0):
         k1=1
+        print("BALL HITS TOP WALL")
     gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)    
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)  
     for (x, y, w, h) in faces:
-        cv2.rectangle(frm, (x, y), (x+w, y+h), (255, 0, 0), 2)   
+        cv2.rectangle(frm, (x, y), (x+w, y+h), (255, 0, 0), -1)   
     for i in range(img2.shape[0]):
         for j in range(img2.shape[1]):
             for k in range(3):
-                img3[i+xi+10*k1][j+yi+10*k2][k] = img[i][j][k]
-                  
-    
-    xi+=k1*10
-    yi+=k2*10
-    print(x,y,x+w,y+h)
+                img3[i+xi+20*k1][j+yi+20*k2][k] = img[i][j][k]
+                     
+    xi+=k1*20
+    yi+=k2*20
+   # print(x,y,x+w,y+h,xi+hi,yi+ki)
    
     hi=img2.shape[0]-1
     ki=img2.shape[1]-1
-     
+   
     t += 1
     Combined = cv2.addWeighted(frm, 0.7,img3, 0.3, 0.1)
     cv2.imshow("window",Combined)
